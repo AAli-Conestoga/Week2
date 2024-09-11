87ab1f9032a84da2870c0cc03eb9d37b46461448
AAli-Conestoga <Akhan4290@conestogac.on.ca>
2024-09-11 16:11:00 -0400

    Commit Sum of numbers

---
+++ SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number : ");
+        int firstNum =int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number : ");
+        int secondNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the third number :");
+        int thirdNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the fourth number : ");
+        int fourthNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the fifth number :");
+        int fifthNum = int.Parse(Console.ReadLine());
+
+        int sum = firstNum + secondNum + thirdNum + fourthNum + fifthNum;
+
+        Console.WriteLine("The sum of the five numbers are : " + sum);
+    }
+}
